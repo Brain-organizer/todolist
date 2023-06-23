224d21ef31b107b0959ba856d76c072bc69388e3
Yongjin An <yongjin5352@gmail.com>
2023-06-23 18:35:27 +0900

    전체를 감싸는 div와 title 완성

---
+++ src/Title.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Title.css'
+
+function Title() {
+  
+  return (
+    <div className='title-bar'>
+      <div className='title'>To do list</div>
+      <div className='library'>react</div>
+    </div>
+  )
+}
+
+export default Title
